06cb0b2d0f27bb2d7e49eac6a860cb4498652749
jjencina <91247078+jjencina@users.noreply.github.com>
2022-02-10 10:24:26 +0100

    Update Junction.java

--- src/simulator/model/Junction.java
+++ src/simulator/model/Junction.java
@@ -1,12 +1,41 @@
 package simulator.model;
 
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONObject;
 
 public class Junction extends SimulatedObject {
 
-	Junction(String id) {
-		super(id);
-		// TODO Auto-generated constructor stub
+	private List<Road> enteringRoadList;
+	private Map<Junction,Road> leavingRoadMap;
+	private List<List<Vehicle>> queueList;
+	private Map<Road,List<Vehicle>> queueRoad; //Lo recomienda por eficiencia pero no es indispensable
+	private int greenLightIndex;
+	private int lastStepLightChange;
+	//private LightSwitchStrategy lsStrategy;
+	//private DequeingStrategy dqStrategy;
+	private int coor; //No se usan esta práctica
+	
+	//Junction(String id, LightSwitchStrategy lsStrategy, DequeingStrategy dqStrategy, int xCoor, int yCoor)
+	Junction(String id){
+			super(id);
+			
+			if (lsStrategy == null) {
+				throw new IllegalArgumentException("LightSwitchStrategy can't be null");
+			}
+			
+			if (dqStrategy == null) {
+				throw new IllegalArgumentException("LightSwitchStrategy can't be null");
+			}
+			
+			if () {
+				throw new IllegalArgumentException("itinerary size must be at least 2");
+			}
+			
+			greenLightIndex = -1;
+			lastStepLightChange = 0;
+			
 	}
 	
 	void addIncommingRoad(Road r) {
