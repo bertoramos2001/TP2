a10efc45525e9c34b6c9a9294fbac04a2be589f0
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2022-03-18 13:58:13 +0100

    pasado control panel a su sitio

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -1,18 +1,10 @@
 package simulator.view;
 
-import java.awt.Dimension;
-import java.awt.Insets;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.io.File;
 import java.util.List;
 
-import javax.swing.Box;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JToolBar;
-import javax.swing.SpinnerNumberModel;
+import javax.swing.*;
 
 import simulator.control.Controller;
 import simulator.model.Event;
@@ -20,14 +12,14 @@ import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
 public class ControlPanel extends JPanel implements TrafficSimObserver {
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	ControlPanel(Controller ctrl) {
-
+		ctrl.addObserver(this);
+		initGUI();
 	}
 	
 	@Override
@@ -65,6 +57,79 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	 private void initGUI() {
+		 this.setLayout(new BorderLayout());
+		JPanel controlPanel = new JPanel(new BorderLayout());
+		this.add(controlPanel);
+		
+		JToolBar myToolBar = new JToolBar();
+		controlPanel.add(myToolBar);
+		
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new File("./resources/examples"));
+		
+		JButton folderButton = new JButton();
+		folderButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/open.png")));
+		folderButton.setToolTipText("Open a file");
+		myToolBar.add(folderButton);
+		folderButton.addActionListener((e) -> {loadFile(fc);});
+		
+		myToolBar.addSeparator();
+		
+		JButton contClassButton = new JButton();
+		contClassButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/co2class.png")));
+		contClassButton.setToolTipText("Change CO2 Class of a Vehicle");
+		myToolBar.add(contClassButton);
+		//listener del boton
+		
+		JButton weatherButton = new JButton();
+		weatherButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/weather.png")));
+		weatherButton.setToolTipText("Change Weather of a Road");
+		myToolBar.add(weatherButton);
+		//listener del boton
+		
+		myToolBar.addSeparator();
+		
+		JButton playButton = new JButton();
+		playButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/run.png")));
+		playButton.setToolTipText("Run the simulator");
+		myToolBar.add(playButton);
+		//listener del boton
+		
+		JButton stopButton = new JButton();
+		stopButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/stop.png")));
+		stopButton.setToolTipText("Stop the simulator");
+		myToolBar.add(stopButton);
+		//listener del boton
+		
+		JLabel ticks = new JLabel("Ticks");
+		myToolBar.add(ticks);
+		
+		JSpinner contTicks = new JSpinner(new SpinnerNumberModel(1, 1, 1000, 1));
+		contTicks.setMaximumSize(new Dimension(50, 50));
+		myToolBar.add(contTicks);
+		
+		myToolBar.add(Box.createGlue());
+		myToolBar.addSeparator();
+		
+		JButton exitButton = new JButton();
+		exitButton.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage("resources/icons/exit.png")));
+		exitButton.setToolTipText("Exit the simulator");
+		myToolBar.add(exitButton);
+		//cerrar la aplicacion
+		
+		myToolBar.setMargin(new Insets(5, 5, 5, 5));
+	}
+	
+		//TODO: este metodo hay que cambiarlo a control panel junto a todo lo de la toolbar, preguntar como se hace
+		private void loadFile(JFileChooser fc) {
+			int i = fc.showOpenDialog(this);
+			if (i == JFileChooser.APPROVE_OPTION) {
+				//TODO: aqui añadir el archivo a ejecucion
+				System.out.println(fc.getSelectedFile());
+			}
+		}
 	
 
 }
